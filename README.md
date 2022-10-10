757980fb06873d2ab834bacf10ccd46167569467
Lukas <lukasstrassel@googlemail.com>
2022-10-10 12:38:45 +0200

    docs: add a bit of docs

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # AAVE paraswap collector
 
-Contract that allows claiming paraswap rewards to the aave treasury.
+Paraswap rewards part of positive slippage to the referrer which in case of swaps performed trough the decentralized ipfs aave interface is the aave protocol itself.
+Currently these rewards can be claimed trough aave governance & respective guardians which is suboptimal as claiming has to go trough governance or be coordinated with a multisig.
+
+The contract presented here aims to simplify this process by allow anyone to claim to the aave treasury.
--- src/contracts/AaveParaswapCollector.sol
+++ src/contracts/AaveParaswapCollector.sol
@@ -6,15 +6,15 @@ import {IFeeClaimer} from '../interfaces/IFeeClaimer.sol';
 import {IERC20} from '../interfaces/IERC20.sol';
 
 /**
- * Helper contract that allows claiming paraswap partner fee to the AAVE_TREASURY on the current network.
+ * Helper contract that allows claiming paraswap partner fee to the collector on the respective network.
  */
 contract AaveParaswapCollector {
-  address public immutable AAVE_TREASURY;
-  IFeeClaimer public immutable PARASWAP_FEE_CLAIMER;
+  address public immutable collector;
+  IFeeClaimer public immutable paraswapFeeClaimer;
 
-  constructor(address treasury, address paraswapFeeClaimer) {
-    AAVE_TREASURY = treasury;
-    PARASWAP_FEE_CLAIMER = IFeeClaimer(paraswapFeeClaimer);
+  constructor(address _collector, address _paraswapFeeClaimer) {
+    collector = _collector;
+    paraswapFeeClaimer = IFeeClaimer(_paraswapFeeClaimer);
   }
 
   /**
@@ -25,22 +25,22 @@ contract AaveParaswapCollector {
     view
     returns (uint256[] memory)
   {
-    return PARASWAP_FEE_CLAIMER.batchGetBalance(assets, address(this));
+    return paraswapFeeClaimer.batchGetBalance(assets, address(this));
   }
 
   /**
-   * @dev withdraws a single asset to the aave treasury
+   * @dev withdraws a single asset to the collector
    * @notice will revert when there's nothing to claim
    */
-  function claimToTreasury(IERC20 asset) external {
-    PARASWAP_FEE_CLAIMER.withdrawAllERC20(asset, AAVE_TREASURY);
+  function claimToCollector(IERC20 asset) external {
+    paraswapFeeClaimer.withdrawAllERC20(asset, collector);
   }
 
   /**
-   * @dev withdraws all asset to the aave treasury
+   * @dev withdraws all asset to the collector
    * @notice will revert when there's nothing to claim on a single supplied asset
    */
-  function batchClaimToTreasury(address[] memory assets) external {
-    PARASWAP_FEE_CLAIMER.batchWithdrawAllERC20(assets, AAVE_TREASURY);
+  function batchClaimToCollector(address[] memory assets) external {
+    paraswapFeeClaimer.batchWithdrawAllERC20(assets, collector);
   }
 }
